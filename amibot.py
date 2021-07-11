f2e935be3d935c0f062ea46bbd7507300cf40c9b
r0ckstaRf <levkametov1@gmail.com>
2021-07-11 18:22:09 +0300

    Добавлены новые команды

--- amibot.py
+++ amibot.py
@@ -26,7 +26,46 @@ for i in admx:
 		print("invalid admin links/format")
 subclient=amino.SubClient(comId=cid,profile=client.profile)
 
+WARNS = []
+ANTI_SPAM = {}
+JOIN_LEAVE_DETECTOR = {}
+
 print("inside community")
+
+def on_message(data):
+    user_id = data.message.author.userId
+    if user_id != client.userId:
+        msg_type = data.message.type
+        if msg_type != 0:
+            media_type = data.message.mediaType
+            content = data.message.content
+            if content is not None and media_type == 0:
+                sub_client.kick(chatId=data.message.chatId, userId=user_id, allowRejoin=False)
+                print(f"{user_id} удален из чата за отправку системного сообщения")
+
+@client.event("on_image_message")
+@client.event("on_text_message")
+def on_antispam(data):
+    user_id = data.message.author.userId
+    if user_id != client.userId:
+        if ANTI_SPAM.get(user_id) is None:
+            ANTI_SPAM[user_id] = int(time.time())
+        elif int(time.time()) - ANTI_SPAM[user_id] <= 0.5:
+            if WARNS.count(user_id) >= 4:
+                sub_client.kick(userId=user_id, chatId=data.message.chatId, allowRejoin=False)
+                print(f"{user_id} удален из чата за спам")
+                for i in WARNS:
+                    if i == user_id:
+                        WARNS.remove(user_id)
+            else:
+                WARNS.append(user_id)
+        elif int(time.time()) - ANTI_SPAM[user_id] > 1:
+            ANTI_SPAM[user_id] = int(time.time())
+            for i in WARNS:
+                if i == user_id:
+                    WARNS.remove(user_id)
+
+
 @client.event("on_group_member_join")
 def on_group_member_join(data):
 	if data.comId==cidy:
